d81240bd6e6ac6df6ca078f9f9e3aae404e8cba1
lightvortex <rishishukla242@gmail.com>
2020-10-29 09:33:20 +0530

    msm8996-common:Update audio stack from Daisy

--- msm8996.mk
+++ msm8996.mk
@@ -83,17 +83,22 @@ PRODUCT_PACKAGES += \
 
 # Audio
 PRODUCT_PACKAGES += \
+    android.hardware.audio@2.0-impl \
     android.hardware.audio@2.0-service \
+    android.hardware.audio@5.0 \
     android.hardware.audio@5.0-impl \
+    android.hardware.audio.common@5.0 \
+    android.hardware.audio.common@5.0-util \
+    android.hardware.audio.effect@5.0 \
     android.hardware.audio.effect@5.0-impl \
+    android.hardware.soundtrigger@2.2-impl \
     android.hardware.bluetooth.audio@2.0-impl \
-    android.hardware.soundtrigger@2.1-impl \
-    audio.a2dp.default \
     audio.bluetooth.default \
+    audio.a2dp.default \
     audio.primary.msm8996 \
     audio.r_submix.default \
-    audio.usb.default \
     libaacwrapper \
+    audio.usb.default \
     libaudio-resampler \
     libaudioroute \
     libqcompostprocbundle \
@@ -104,7 +109,6 @@ PRODUCT_PACKAGES += \
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/audio/audio_effects.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_effects.xml \
-    $(LOCAL_PATH)/audio/audio_tuning_mixer.txt:$(TARGET_COPY_OUT_VENDOR)/etc/audio_tuning_mixer.txt \
     $(LOCAL_PATH)/audio/audio_output_policy.conf:$(TARGET_COPY_OUT_VENDOR)/etc/audio_output_policy.conf \
     $(LOCAL_PATH)/audio/audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_policy_configuration.xml \
     $(LOCAL_PATH)/audio/listen_platform_info.xml:system/etc/listen_platform_info.xml \
